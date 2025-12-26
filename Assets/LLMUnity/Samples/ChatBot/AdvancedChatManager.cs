ada7a11843815ecfb93c6aa0e9b4cf9fc4009755
Hui <2963134079@qq.com>
2025-12-26 15:09:07 +0800

    fix:在代码中实例化消息预制体

--- Assets/LLMUnity/Samples/ChatBot/AdvancedChatManager.cs
+++ Assets/LLMUnity/Samples/ChatBot/AdvancedChatManager.cs
@@ -108,6 +108,8 @@ public class AdvancedChatManager : MonoBehaviour
 
     void Start()
     {
+        userMessagePrefab = Resources.Load<GameObject>("Prefabs/UserMessagePreFab Variant");
+        aiMessagePrefab = Resources.Load<GameObject>("Prefabs/AIMessagePreFab Variant");
         // 初始化 - 语音输入按钮点击事件
         voiceInputButton.onClick.AddListener(OnVoiceInputButtonClicked);
 
